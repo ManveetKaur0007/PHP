2051feb11fca3c78610e4d615024bd2d79da34a9
Manveet Kaur <90703104+ManveetKaur0007@users.noreply.github.com>
2022-07-16 21:07:41 -0400

    final commit

---
+++ assignment1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset="UTF-8">
+    <title>Wonders of the World! - {Manveet Kaur!}</title>
+</head>
+<body>
+<?php
+//  Program by: Manveet Kaur
+//  Student Id: W0782002
+//  Date done: 2021/09/18
+
+echo "<p><h2>These are some of the wonders of the world!</h2></p>";
+echo "<ul>";
+echo "<li>The great Pyramids, Giza</li>";
+echo "<li>The great wall of China</li>";
+echo "<li>Roman Colosseum, Greece</li>";
+echo "<li>Taj Mahal, India</li>";
+echo "<li>Machu Picchu, Peru</li>";
+echo ("</ul>");
+
+echo "<i>Images:</i> <br />"; 
+echo '<img src="pyramid.jpg" />';
+echo '<img src="greatwall.jpg" />';
+echo '<img src="greece.jpg" />';
+echo '<img src="taj-mahal-India.jpg" />';
+echo '<img src="machu-picchu.jpg" />';
+
+
+echo("<br /><br />");
+echo("<br /> Programmed by - Manveet Kaur");
+?>
+
+</body>
+</html>
+
+
