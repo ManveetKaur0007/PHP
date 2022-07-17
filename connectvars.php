b82eb7ed32575abfd75bc35654d152f490bebb92
Manveet Kaur <90703104+ManveetKaur0007@users.noreply.github.com>
2022-07-16 21:14:02 -0400

    final commit

---
+++ connectvars.php
@@ -0,0 +1,7 @@
+<?php
+  // Define the database connection constants here!
+ define('DB_HOST', 'localhost');
+ define('DB_USER', 'username');     // your scweb username
+ define('DB_PASSWORD', 'xxxxxxxxxxxxxxxxxxxx');  // See blackboard for 20-char password
+ define('DB_NAME', 'usernamedb');   // username followed by lowercase db
+?>
---
+++ connectvars_before.php
@@ -0,0 +1,7 @@
+<?php
+  // Define the database connection constants here!
+ define('DB_HOST', 'localhost');
+ define('DB_USER', 'USERNAME');     // your scweb username
+ define('DB_PASSWORD', 'xxxxxxxxxxxxxxxxxxxx');  // See blackboard for 20-char password
+ define('DB_NAME', 'USERNAMEdb');   // username followed by lowercase db
+?>
