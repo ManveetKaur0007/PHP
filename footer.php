76bcd9a846cee45c1034cf9daf65abfdeda49b37
Manveet Kaur <90703104+ManveetKaur0007@users.noreply.github.com>
2022-07-16 21:13:29 -0400

    final commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+       <div class="spacer"></div>
+        <footer>
+            <p>Copyright &copy;2019 Developer Jobs<br />
+            Web site designed by {Manveet Kaur}</p>
+        </footer>
+
+    </body>
+</html>
+
