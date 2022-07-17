fe470d01128c8312ebe4290d14d23b87511132b8
Manveet Kaur <90703104+ManveetKaur0007@users.noreply.github.com>
2022-07-16 21:10:20 -0400

    final commit

---
+++ confirmation.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Confirmation</title>
+</head>
+
+<body style="background-color: rgb(229,243,247);">
+<h3>Confirmation</h3>
+<?php
+echo "Your Order has been confirmed";
+
+?>
+</body>
+</html>
